f9ca0c6e1a6e183c4c4ca4bd921932c724fc54ff
John Giles <gilesjohnr@gmail.com>
2024-04-11 17:40:45 -0700

    updates to precip func

--- R/get_precip_data.R
+++ R/get_precip_data.R
@@ -1,14 +1,17 @@
-#' Get precipitation data
+#' Get CHIRPS precipitation data
 #'
 #' This function takes information of where and when a set of environmental samples were
-#' collected and retrieves precipitation data for those locations and times. Data come from
+#' collected and retrieves precipitation data (in millimeters) for those locations and times. Data come from
+#' the Climate Hazards Center InfraRed Precipitation with Station data ([CHIRPS](https://www.chc.ucsb.edu/data)) via the [`chirps`](https://docs.ropensci.org/chirps/) R package.
+#' Additionally, the optional `intervals` argument specifies a set of intervals over which the function will calculate the
+#' cumulative sum of precipitation in millimeters (mm) for the previous X number of days for each location.
 #'
 #' @param lat A numeric vector giving the latitude of the sampling sites in Decimal Degrees.
 #' @param lon A numeric vector giving the longitude of the sampling sites in Decimal Degrees.
 #' @param dates A character or date vector of dates giving the date when each sample was
 #' collected (format is YYYY-MM-DD)
 #' @param intervals An integer vector giving a set of time intervals over which to sum the
-#' precipitation data. Defaults to 1 (returns the precipitation value at time t). If `intervals`=3
+#' precipitation data. Default is NULL where the interval is 0 (returns the precipitation value at time t). If `intervals`=3
 #' then the cumulative precipitation over the preceding 3 days is returned.
 #'
 #' @returns data.frame
@@ -39,42 +42,6 @@
 #'
 #' }
 
-if (F) {
-
-     lon <- template_es_data$lon
-     lat <- template_es_data$lat
-     dates <- template_es_data$date
-     intervals <- 1
-
-     lon <- ll$lon
-     lat <- ll$lat
-     dates <- c("2017-12-01", "2017-12-31")
-
-     ll <- data.frame(lon = c(-56.0281, -54.9857),
-                      lat = c(-2.9094, -2.8756))
-
-     d <- get_precip_data(lon=ll$lon,
-                          lat=ll$lat,
-                          dates = c("2017-12-01", "2017-12-31"),
-                          intervals = c(1,3,7))
-
-     head(d)
-
-     ggplot2::ggplot(d, aes(x = date)) +
-          geom_line(aes(y = precip_mm_sum_7, col='Cumulative sum 7 days')) +
-          geom_line(aes(y = precip_mm_sum_3, col='Cumulative sum 3 days')) +
-          geom_line(aes(y = precip_mm_sum_1, col='Cumulative sum 1 day')) +
-          geom_line(aes(y = precip_mm)) +
-          facet_grid(rows=vars(id)) +
-          labs(x="", y = "Precipitation (mm)") +
-          theme_bw() +
-          theme(legend.position = 'bottom',
-                legend.title = element_blank())
-
-
-
-}
-
 get_precip_data <- function(lon,
                             lat,
                             dates,
@@ -102,7 +69,7 @@ get_precip_data <- function(lon,
      # Download precip data from Climate Hazards Group server
      message(glue::glue("Total locations = {n_locations}"))
      message(glue::glue("Date range = {paste(c(min(dates), max(dates)), collapse=' -- ')}"))
-     message("Downloading precipitation data via 'chirps' package")
+     message("Downloading CHIRPS precipitation data via 'chirps' package")
 
      data_chirps <- chirps::get_chirps(object=tmp,
                                        dates=as.character(date_range),
@@ -155,9 +122,7 @@ get_precip_data <- function(lon,
      sel_rows <- which(data_chirps$date < min(dates))
      data_chirps[sel_rows, sel_cols] <- NA
      data_chirps <- data_chirps[complete.cases(data_chirps),]
-
      colnames(data_chirps)[colnames(data_chirps) == 'chirps'] <- 'precip_mm'
-
 
      return(as.data.frame(data_chirps))
 
