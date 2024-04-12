6e0ccbe758a634bb2b3f1353c2a5d02f4c7ba72c
John Giles <gilesjohnr@gmail.com>
2024-04-12 14:50:51 -0700

    adding elevation for xy points func

---
+++ R/get_elevation_data.R
@@ -0,0 +1,54 @@
+#' Get elevation data
+#'
+#' This function takes information of where and when a set of environmental samples were
+#' collected and retrieves the elevation (in meters) for those locations at an approximate 100m spatial resolution.
+#' Data come from the [SRTM](https://www.usgs.gov/centers/eros/science/usgs-eros-archive-digital-elevation-shuttle-radar-topography-mission-srtm-1)
+#' DEM which are accessed through the Amazon Web Services (AWS) API and the [`elevatr`](https://cran.r-project.org/web/packages/elevatr/index.html)
+#' R package.
+#'
+#' @param lat A numeric vector giving the latitude of the sampling sites in Decimal Degrees.
+#' @param lon A numeric vector giving the longitude of the sampling sites in Decimal Degrees.
+#'
+#' @returns data.frame
+#'
+#' @examples
+#' \dontrun{
+#'
+#' get_elevation_data(lon = template_es_data$lon,
+#'                    lat = template_es_data$lat)
+#'
+#' }
+
+get_elevation_data <- function(lon,
+                               lat
+){
+
+     # Checks
+     check <- length(lat) == length(lon)
+     if (!check) stop('lat and lon args must be equal in length')
+
+     # Get distinct coordinate sets
+     unique_lonlat <- data.frame(x=lon, y=lat)
+     unique_lonlat <- dplyr::distinct(unique_lonlat)
+     n_locations <- nrow(unique_lonlat)
+
+     # Download precip data from Climate Hazards Group server
+     message(glue::glue("Total locations = {n_locations}"))
+     message("Downloading elevation data from AWS API via elevatr package ...")
+
+     wgs_proj_string <- sp::CRS('+init=epsg:4326 +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0')
+
+     data_elev <- elevatr::get_elev_point(locations = unique_lonlat,
+                                          prj = sf::st_crs(wgs_proj_string),
+                                          src = 'aws',
+                                          z = 10)
+
+     out <- data.frame(
+          lon = unique_lonlat$x,
+          lat = unique_lonlat$y,
+          elevation = data_elev$elevation
+     )
+
+     return(out)
+
+}
---
+++ R/get_river_discharge_data.R
@@ -0,0 +1,82 @@
+#' Get river discharge data
+#'
+#' This function takes information of where and when a set of environmental samples were
+#' collected and retrieves daily river discharge data from the nearest river (\eqn{m^3/s}) for those locations and times. Data come from
+#' the Open-Meteo Global Flood API ([https://open-meteo.com/en/docs/flood-api](https://open-meteo.com/en/docs/flood-api))
+#' via the [`openmeteo`](https://cran.r-project.org/web/packages/openmeteo/index.html) R package.
+#'
+#' @param lat A numeric vector giving the latitude of the sampling sites in Decimal Degrees.
+#' @param lon A numeric vector giving the longitude of the sampling sites in Decimal Degrees.
+#' @param dates A character or date vector of dates giving the date when each sample was
+#' collected (format is YYYY-MM-DD)
+#'
+#' @returns data.frame
+#'
+#' @examples
+#' \dontrun{
+#'
+#' d <- get_river_discharge_data(lon = c(-54.9857, -52.9857),
+#'                               lat = c(-10.9094, -25.8756),
+#'                               dates = c("2020-06-01", "2020-10-31"))
+#'
+#' head(d)
+#'
+#' ggplot2::ggplot(d, aes(x = date)) +
+#'      geom_line(aes(y = daily_river_discharge)) +
+#'      facet_grid(rows=vars(id)) +
+#'      labs(x="", y = "Local River Discharge (m^3/s)") +
+#'      theme_bw() +
+#'      theme(legend.position = 'bottom',
+#'            legend.title = element_blank())
+#'
+#' }
+
+get_river_discharge_data <- function(lon,
+                                     lat,
+                                     dates
+){
+
+     # Checks
+     check <- length(lat) == length(lon) & length(lat) == length(dates)
+     if (!check) stop('lat, lon, and dates must be equal in length')
+
+     dates <- as.Date(dates, format='%Y-%m-%d')
+     if (all(is.na(dates))) stop('Cannot identify date format')
+
+     # Get distinct coordinate sets
+     unique_latlon <- data.frame(lat, lon)
+     unique_latlon <- dplyr::distinct(unique_latlon)
+     date_range <- c(min(dates), max(dates))
+
+     n_locations <- nrow(unique_latlon)
+     n_dates <- (date_range[2] - date_range[1]) + 1
+
+     # Download precip data from Climate Hazards Group server
+     message(glue::glue("Total locations = {n_locations}"))
+     message(glue::glue("Date range = {paste(c(min(dates), max(dates)), collapse=' -- ')}"))
+     message("Downloading daily river discharge data from the Global Flood API...")
+
+     data_river <- data.frame()
+
+     for (i in 1:nrow(unique_latlon)) {
+
+          tmp <- openmeteo::river_discharge(
+               location = c(unique_latlon$lat[i], unique_latlon$lon[i]),
+               start = date_range[1],
+               end = date_range[2],
+               daily = "river_discharge"
+          )
+
+          data_river <- rbind(
+               data_river,
+               data.frame(id = i,
+                          lat = lat[i],
+                          lon = lon[i],
+                          tmp)
+          )
+
+     }
+
+     return(data_river)
+
+}
