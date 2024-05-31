7133b1d274159d68518e38fee75bc6ec33aa0c83
John Giles <gilesjohnr@gmail.com>
2024-05-30 21:47:46 -0700

    first round updates to get_admin_data, geodata archived

--- R/download_admin_data.R
+++ R/download_admin_data.R
@@ -1,61 +1,64 @@
-#' Download administrative boundaries from GADM
+#' Download administrative boundaries from geoBoundaries API
 #'
-#' This function takes a single ISO country code, or vector of multiple ISO country codes, and downloads the corresponding
-#' high resolution administrative boundary shapefiles from [https://gadm.org/](https://gadm.org/) via
-#' the [`geodata::gadm`](https://rdrr.io/github/rspatial/geodata/man/gadm.html) function. The most detailed administrative
-#' units are returned (level 4 for most countries).
+#' This function takes a single ISO country code and downloads the corresponding
+#' high resolution administrative boundary [GeoJSON](https://en.wikipedia.org/wiki/GeoJSON) files from the [www.geoBoundaries.org](https://www.geoboundaries.org/) API
+#' hosted at GitHub [HERE](https://github.com/wmgeolab/geoBoundaries/tree/9469f09592ced973a3448cf66b6100b741b64c0d). If the
+#' desired administrative level is not available the next most detailed administrative level is returned.
 #'
 #' @param iso3 A three-letter capitalized character string. Must follow the ISO-3166 Alpha-3 country code
-#' standard ([https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3](https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3)). Can accept a vector of multiple ISO codes.
+#' @param admin_level An integer specifying the administrative level. It should be between 0 and 5.
+#' standard ([https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3](https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3)).
+#' @param release A character string specifying the release type on the geoBoundaries API. It should be one of 'gbOpen',
+#' 'gbHumanitarian', or 'gbAuthoritative'.  Release types are described at [https://www.geoboundaries.org/api.html#api](https://www.geoboundaries.org/api.html#api).
 #' @param path_output A character string giving the file path of an output directory to save downloaded data.
+#' @param simplified Logical indicating whether to download simplified administrative boundaries instead of high resolution. Default is FALSE.
 #'
-#' @returns NULL
+#' @returns NULL. Administrative boundaries are downloaded in .geojson format.
 #'
 #' @examples
+#'
 #' \dontrun{
-#'
-#' download_admin_data(iso3 = c('FRA', 'ITA'), path_output = getwd())
-#'
+#' download_admin_data(iso3 = 'ITA',
+#'                     admin_level = 2,
+#'                     release = 'gbOpen',
+#'                     path_output = getwd())
 #' }
+#'
 
 download_admin_data <- function(iso3,
-                                path_output
+                                admin_level,
+                                release,
+                                path_output,
+                                simplified=FALSE
 ) {
 
      if (!is.character(iso3)) stop('iso3 code(s) must be character')
      if (!dir.exists(path_output)) stop('path_output does not exist')
 
-     message("Getting administrative shapefiles for...")
+     release_map <- c('gbOpen', 'gbHumanitarian', 'gbAuthoritative')
+     if (!(release %in% release_map)) stop(paste("Error: release should be one of", paste(release_map, collapse = ', ')))
+     if (admin_level < 0 | admin_level > 5) stop("Error: admin_level should be an integer between 0 and 5")
 
-     for (i in 1:length(iso3)) {
+     # Get metadata from geoBoundaries API
+     api_data <- es::get_geoboundaries_api_data(iso3=iso3,
+                                                admin_level=admin_level,
+                                                release=release)
 
-          message(iso3[i])
+     url <- ifelse(simplified,
+                   api_data$simplifiedGeometryGeoJSON,
+                   api_data$gjDownloadURL)
 
-          message("trying level 5...")
-          admins <- geodata::gadm(country=iso3[i], level=5, resolution=1, version='latest', path=path_output)
-
-          if (is.null(admins)) {
-               message("trying level 4...")
-               admins <- geodata::gadm(country=iso3[i], level=4, resolution=1, version='latest', path=path_output)
-          }
-
-          if (is.null(admins)) {
-               message("trying level 3...")
-               admins <- geodata::gadm(country=iso3[i], level=3, resolution=1, version='latest', path=path_output)
-          }
-
-          if (is.null(admins)) {
-               message("trying level 2...")
-               admins <- geodata::gadm(country=iso3[i], level=2, resolution=1, version='latest', path=path_output)
-          }
-
-          if (is.null(admins)) {
-               message("trying level 1...")
-               admins <- geodata::gadm(country=iso3[i], level=1, resolution=1, version='latest', path=path_output)
-          }
-
-     }
+     # Download to path_output
+     download.file(url = url,
+                   destfile = file.path(path_output, basename(url)),
+                   method='auto',
+                   quiet = FALSE,
+                   mode = "wb",
+                   cacheOK = TRUE,
+                   extra = getOption("download.file.extra"))
 
      message('Done.')
-     message(glue::glue("Data saved here: {path_output}/gadm"))
+     message(glue::glue("Data saved here: {path_output}/{basename(url)}"))
+     return(file.path(path_output, basename(url)))
+
 }
---
+++ R/get_geoboundaries_api_data.R
@@ -0,0 +1,60 @@
+#' Request metadata from GeoBoundaries API
+#'
+#' This function retrieves GeoBoundaries data from the API based on the specified release, ISO3 country code, and administrative level.
+#' If data is not found at the specified administrative level, it attempts to retrieve data from a lower administrative level until data
+#' is found or the lowest level is reached.
+#'
+#' @param iso3 A three-letter capitalized character string. Must follow the ISO-3166 Alpha-3 country code
+#' standard ([https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3](https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3)).
+#' @param admin_level An integer specifying the administrative level. It should be between 0 and 5.
+#' @param release A character string specifying the release type. It should be one of 'gbOpen', 'gbHumanitarian', or 'gbAuthoritative'. Default is 'gbOpen'.
+#'
+#' @return A list containing the GeoBoundaries API data and file paths to admin boundaries in .geojson format.
+#'
+#' @examples
+#'
+#' \dontrun{
+#' tmp <- .get_geoboundaries_api_data('gbOpen', 'ITA', 2)
+#' print(tmp)
+#' }
+#'
+
+get_geoboundaries_api_data <- function(iso3,
+                                       admin_level,
+                                       release='gbOpen'
+) {
+
+     release_map <- c('gbOpen', 'gbHumanitarian', 'gbAuthoritative')
+     if (!(release %in% release_map)) stop(paste("Error: release should be one of", paste(release_map, collapse = ', ')))
+     if (admin_level < 0 | admin_level > 5) stop("Error: admin_level should be an integer between 0 and 5")
+
+     admin_level_map <- c("ADM0", "ADM1", "ADM2", "ADM3", "ADM4", "ADM5")
+     base_url <- "https://www.geoboundaries.org/api/current"
+
+     while (admin_level >= 0) {
+
+          message(paste("Trying admin level", admin_level))
+          admin_level_str <- admin_level_map[admin_level + 1]
+          api_url <- paste0(base_url, "/", release, "/", iso3, "/", admin_level_str, "/")
+          response <- httr::GET(api_url)
+
+          if (response$status_code == 200) {
+
+               data <- httr::content(response, "text")
+               data_parsed <- jsonlite::fromJSON(data)
+               return(data_parsed)
+
+          } else if (response$status_code == 404) {
+
+               admin_level <- admin_level - 1 # Try lower admin level
+
+          } else {
+
+               stop("Error: ", response$status_code)
+
+          }
+     }
+
+     stop("Error: No valid data found at any admin level.")
+
+}
